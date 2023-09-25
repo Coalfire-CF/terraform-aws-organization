05409762b34955a923d3edd3d5757944a2720a59
kmobl <moblkourosh@gmail.com>
2023-09-25 13:49:37 -0500

    addressed @oleg's comments

--- security_hub.tf
+++ security_hub.tf
@@ -13,7 +13,7 @@ resource "aws_securityhub_organization_configuration" "sechub_org_config" {
 
 resource "aws_securityhub_standards_subscription" "cis" {
   for_each = var.aws_sec_hub_standards_arn
-  depends_on    = [aws_securityhub_organization_admin_account.sechub_org_admin]
+  depends_on    = [aws_securityhub_organization_admin_account.sechub_org_admin[0]]
   standards_arn = each.value
 }
 
