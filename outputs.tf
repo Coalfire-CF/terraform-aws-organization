efaed5a5e6854e376be223eb59dcb8dd8e04b442
kmobl <moblkourosh@gmail.com>
2023-10-03 19:19:15 -0500

    Initial Release

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "accounts" {
+  value = aws_organizations_organization.org.accounts
+  description = "List of org accounts including master"
+}
+
+resource "aws_organizations_delegated_administrator" "delegated" {
+  account_id        = var.delegated_account_id
+  service_principal = var.delegated_service_principal
+}
