97250754139dd02fcc7f7ebc262a49d5d6043f8c
Thomas Kennedy <101284566+tkennedy-cf@users.noreply.github.com>
2024-01-03 14:15:26 -0500

    more org fixes

--- org.tf
+++ org.tf
@@ -10,7 +10,7 @@ resource "aws_organizations_delegated_administrator" "delegated_admin" {
   for_each = toset(var.delegated_admin_account_id)
 
   account_id        = each.key
-  service_principal = var.delegated_service_principal[each.value]
+  service_principal = var.delegated_service_principal
 }
 
 resource "aws_organizations_account" "account" {
