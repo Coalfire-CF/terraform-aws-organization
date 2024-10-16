72d6925ab28b3218fefe20a25c6f008dfbd367c5
Matthew Pappas <matthew.pappas@coalfire.com>
2024-10-16 13:14:02 -0700

    updates

--- org.tf
+++ org.tf
@@ -6,26 +6,26 @@ resource "aws_organizations_organization" "org" {
   #enabled_policy_types = var.enabled_policy_types # I want to implement this based off a check of feature_set - if not set to ALL then this is null.
 }
 
-resource "aws_organizations_delegated_administrator" "delegated_admin" {
-  for_each = toset(var.delegated_admin_account_id)
+# resource "aws_organizations_delegated_administrator" "delegated_admin" {
+#   for_each = toset(var.delegated_admin_account_id)
 
-  account_id        = each.key
-  service_principal = var.delegated_service_principal
-}
+#   account_id        = each.key
+#   service_principal = var.delegated_service_principal
+# }
 
-resource "aws_organizations_account" "account" {
-  for_each = toset(var.aws_new_member_account_email)
+# resource "aws_organizations_account" "account" {
+#   for_each = toset(var.aws_new_member_account_email)
 
-  name  = var.org_account_name
-  email = each.value
-}
+#   name  = var.org_account_name
+#   email = each.value
+# }
 
-resource "aws_organizations_organizational_unit" "ou" {
-  for_each = var.ou_creation_info
+# resource "aws_organizations_organizational_unit" "ou" {
+#   for_each = var.ou_creation_info
 
-  name      = each.value["ou_name"]
-  parent_id = each.value["ou_parent_id"]
-}
+#   name      = each.value["ou_name"]
+#   parent_id = each.value["ou_parent_id"]
+# }
 
 resource "aws_organizations_policy" "scp" {
   content = data.aws_iam_policy_document.scp.json
