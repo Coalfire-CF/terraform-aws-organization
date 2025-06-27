d0b60fd06228307419fe46c4ff2bbed1ca39ddb5
madison.hersom.ctr-at-661095517404 <madison.hersom.ctr@ad.absolutegov.net>
2025-06-27 16:34:02 -0400

    added cloudtrail s3 arn

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "master_account_id" {
   value       = aws_organizations_organization.org.master_account_id
   description = "Master account ID"
 }
+
+output "s3_cloudtrail_arn" {
+  description = "ARN of the CloudTrail S3 bucket"
+  value       = aws_s3_bucket.cloudtrail.arn
+}
